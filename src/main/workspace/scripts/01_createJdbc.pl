d0d0418481c5369e29119dd73669bd17024b43e4
Danilo Bretschneider <git@bretsch.net>
2013-10-22 23:19:27 +0200

    Add modified properties file and modified perl script JDBC connection

--- src/main/workspace/scripts/01_createJdbc.pl
+++ src/main/workspace/scripts/01_createJdbc.pl
@@ -4,14 +4,18 @@ use Config::Simple;
 #use warnings;
 
 my $properties = new Config::Simple('00_properties.ini');
-my $url = $properties->param('jdbc.Url');
-my $user = $properties->param('jdbc.User');
-my $password = $properties->param('jdbc.Password');
+my $url = $properties->param('jdbc.url');
+my $user = $properties->param('jdbc.user');
+my $password = $properties->param('jdbc.password');
 
-print "<?xml version='1.0' encoding='UTF-8'?>\n";
-print "<JDBCConnection configVersion='3.0.0' xmlns='http://www.deegree.org/jdbc' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.deegree.org/jdbc http://schemas.deegree.org/jdbc/3.0.0/jdbc.xsd'>\n";
-	print "\t<Url>". $url ."</Url>\n";
-	print "\t<User>". $user ."</User>\n";
-	print "\t<Password>". $password ."</Password>\n";
-	print "\t<ReadOnly>false</ReadOnly>\n";
-print "</JDBCConnection>";
+my $jdbc_filename = $properties->param('filename.jdbc');
+
+open output,">../jdbc/$jdbc_filename" or die "Can't open the output file! See";
+
+print output "<?xml version='1.0' encoding='UTF-8'?>\n";
+print output "<JDBCConnection configVersion='3.0.0' xmlns='http://www.deegree.org/jdbc' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.deegree.org/jdbc http://schemas.deegree.org/jdbc/3.0.0/jdbc.xsd'>\n";
+	print output "\t<Url>". $url ."</Url>\n";
+	print output "\t<User>". $user ."</User>\n";
+	print output "\t<Password>". $password ."</Password>\n";
+	print output "\t<ReadOnly>false</ReadOnly>\n";
+print output "</JDBCConnection>";
