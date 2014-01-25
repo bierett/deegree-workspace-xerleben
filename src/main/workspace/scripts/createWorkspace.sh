e64a65267a05f265e7c96f1db9857ed5ad1698ac
Danilo Bretschneider <bretschneider@geospatialinformation.org>
2014-01-26 00:17:08 +0100

    Add log message

--- src/main/workspace/scripts/createWorkspace.sh
+++ src/main/workspace/scripts/createWorkspace.sh
@@ -1,59 +1,95 @@
 #!/bin/sh
 
-#Löschen von JDBC
+echo "";
+
+#JDBC
     dir="../jdbc/"
-    echo "Lösche Verzeichnis '$dir'.";
+    echo "[$(date)] ""Lösche Verzeichnis '$dir'.";
     rm -rf $dir/*
     if [ "$(ls -A $dir)" ]; then
-         echo "Löschen ist fehlgeschlagen.\n"
+         echo "[$(date)] ""Löschen ist fehlgeschlagen."
     else
-        echo "Löschen war erfolgreich.\n"
+        echo "[$(date)] ""Löschen war erfolgreich."
     fi
 
-#Löschen von Layers
+    echo "[$(date)] ""Starte JDBC Konfiguration.";
+    perl bin/01_createJdbc.pl
+    if [ "$(ls -A $dir)" ]; then
+         echo "[$(date)] ""JDBC Konfiguration war erfolgreich.\n"
+    else
+        echo "[$(date)] ""JDBC Konfiguration ist fehlgeschlagen.\n"
+    fi
+
+#Layers
     dir="../layers/"
-    echo "Lösche Verzeichnis '$dir'.";
+    echo "[$(date)] ""Lösche Verzeichnis '$dir'.";
     rm -rf $dir/*
     if [ "$(ls -A $dir)" ]; then
-         echo "Löschen ist fehlgeschlagen.\n"
+         echo "[$(date)] ""Löschen ist fehlgeschlagen."
     else
-        echo "Löschen war erfolgreich.\n"
+        echo "[$(date)] ""Löschen war erfolgreich."
     fi
 
-#Löschen von Services
+    echo "[$(date)] ""Starte Layers Konfiguration.";
+    perl bin/04_createLayer.pl
+    if [ "$(ls -A $dir)" ]; then
+         echo "[$(date)] ""Layers Konfiguration war erfolgreich.\n"
+    else
+        echo "[$(date)] ""Layers Konfiguration ist fehlgeschlagen.\n"
+    fi
+
+#Services
     dir="../services/"
-    echo "Lösche Verzeichnis '$dir'.";
+    echo "[$(date)] ""Lösche Verzeichnis '$dir'.";
     rm -rf $dir/*
     if [ "$(ls -A $dir)" ]; then
-         echo "Löschen ist fehlgeschlagen.\n"
+         echo "[$(date)] ""Löschen ist fehlgeschlagen."
     else
-        echo "Löschen war erfolgreich.\n"
+        echo "[$(date)] ""Löschen war erfolgreich."
     fi
 
-#Löschen von Styles
+    echo "[$(date)] ""Starte Services Konfiguration.";
+    perl bin/02_createWfs.pl
+    perl bin/07_createWms.pl
+    perl bin/08_createMetada.pl
+    if [ "$(ls -A $dir)" ]; then
+         echo "[$(date)] ""Services Konfiguration war erfolgreich.\n"
+    else
+        echo "[$(date)] ""Services Konfiguration ist fehlgeschlagen.\n"
+    fi
+
+#Styles
     dir="../styles/"
-    echo "Lösche Verzeichnis '$dir'.";
+    echo "[$(date)] ""Lösche Verzeichnis '$dir'.";
     rm -rf $dir/*
     if [ "$(ls -A $dir)" ]; then
-         echo "Löschen ist fehlgeschlagen.\n"
+         echo "[$(date)] ""Löschen ist fehlgeschlagen."
     else
-        echo "Löschen war erfolgreich.\n"
+        echo "[$(date)] ""Löschen war erfolgreich."
     fi
 
-#Löschen von Themes
+    echo "[$(date)] ""Starte Styles Konfiguration.";
+    perl bin/06_createStyles.pl
+    if [ "$(ls -A $dir)" ]; then
+         echo "[$(date)] ""Styles Konfiguration war erfolgreich.\n"
+    else
+        echo "[$(date)] ""Styles Konfiguration ist fehlgeschlagen.\n"
+    fi
+
+#Themes
     dir="../themes/"
-    echo "Lösche Verzeichnis '$dir'.";
+    echo "[$(date)] ""Lösche Verzeichnis '$dir'.";
     rm -rf $dir/*
     if [ "$(ls -A $dir)" ]; then
-         echo "Löschen ist fehlgeschlagen.\n"
+         echo "[$(date)] ""Löschen ist fehlgeschlagen."
     else
-        echo "Löschen war erfolgreich.\n"
+        echo "[$(date)] ""Löschen war erfolgreich."
     fi
-
-perl bin/01_createJdbc.pl
-perl bin/02_createWfs.pl
-perl bin/04_createLayer.pl
-perl bin/05_createThemes.pl
-perl bin/06_createStyles.pl
-perl bin/07_createWms.pl
-perl bin/08_createMetada.pl
+    
+    echo "[$(date)] ""Starte Themes Konfiguration.";
+    perl bin/05_createThemes.pl
+    if [ "$(ls -A $dir)" ]; then
+         echo "[$(date)] ""Themes Konfiguration war erfolgreich.\n"
+    else
+        echo "[$(date)] ""Themes Konfiguration ist fehlgeschlagen.\n"
+    fi
